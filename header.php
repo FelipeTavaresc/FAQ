c8e08db32e5565511969aca7b0eccf3ec8933467
Felipe Tavares <felipe.tavares@outlook.com>
2018-05-27 19:46:01 -0300

    First Commit

---
+++ header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>FCA</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/fca.css">
+</head>
+<body>
+
+	<div class="navbar navbar-inverse navbar-fixed-top navbar-dark bg-primary">
+		
+		<div class="container">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="index.php">Home</a>
+			</div>
+
+			<div>
+				<ul class="nav navbar-nav">
+					<li><a href="softwares.php">Softwares</a></li>
+					<li><a href="hardwares.php">Hardwares</a></li>
+					<li><a href="utilitarios.php">Utilitários</a></li>
+				</ul>
+			</div>
+		</div>
+
+	</div>
+
+	<div class="container">
+		<div class="principal">
